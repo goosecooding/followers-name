d022b63d3b39d88ada31915abdb8b3f54bae1d0e
Wafik Hamadou <aymenhamadou2000@gmail.com>
2026-02-08 19:37:42 +0100

    day2

--- code.js
+++ code.js
@@ -1,4 +1,4 @@
 const result = document.getElementById("Result")
 function ShowResult() {
-    result.textContent = "there is no name here to search for yet"
+    result.textContent = "the list is too small to search for now"
 }
